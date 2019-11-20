a54cdbfbc885ea8057df97961858cc058ed22b8b
Khalil Najjar <knajjars@gmail.com>
2019-11-20 21:52:36 +0100

    Initial Commit

---
+++ ByteCoin/Model/CoinManager.swift
@@ -0,0 +1,30 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by Angela Yu on 11/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    func getCoinPrice(for currency: String) {
+        if let url = URL(string: "\(baseURL)\(currency)") {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                let dataAsString = String(data: data!, encoding: .utf8)
+                print(dataAsString!)
+            }
+            task.resume()
+        }
+    }
+}
